e03cb1dc71dac60570deaf97b7346368eddca811
Marcel Sinniger <marcel.sinniger@gmail.com>
2013-04-26 15:58:30 +0200

    adsf

---
+++ src/bomberman/Bomberman.java
@@ -0,0 +1,13 @@
+package bomberman;
+
+public class Bomberman {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
