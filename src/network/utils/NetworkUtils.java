d5d3f22ce1ec982bec7a577ed23da19cfd578a7a
Marcel Sinniger <marcel.sinniger@gmail.com>
2013-06-13 12:03:30 +0200

    update network

--- src/network/utils/NetworkUtils.java
+++ src/network/utils/NetworkUtils.java
@@ -1,10 +1,23 @@
 package network.utils;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
+/**
+ * Cette classe met à disposition certaine méthodes générales qui concernent la partie réseau
+ * @author marcel
+ *
+ */
 public class NetworkUtils {
-
+   
+   /**
+    * Cette classe permet de récupérer l'adress IP de l'interface principale
+    * (celui qui est utilisé par la route par défaut)
+    * @return
+    */
    public static String getLocalIp() {
       
       String ipAddress = null;
@@ -18,6 +31,29 @@ public class NetworkUtils {
       }
       
       return ipAddress;
+   }
+   
+   public static void getPrincipalNetworkInterfaces() {
+      //try {
+         
+         
+        // for (Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces(); e.hasMoreElements();)
+        //    final NetworkInterface i = e.nextElement();
+         
+           // if (!i.isLoopback() &&
+           //     !i.isVirtual()) {
+            //   System.out.println(e.nextElement().getInetAddresses().);
+            //}
+
+         
+      //} catch (SocketException e) {
+         // TODO Auto-generated catch block
+        // e.printStackTrace();
+      //}
+   }
+   
+   public static void main(String[] args) {
+      getPrincipalNetworkInterfaces();
    }
 
 
